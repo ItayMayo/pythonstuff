55d15913dd770cd568b9afe1e60245e1c39dc5bd
ItayMayo <itaymayo02@gmail.com>
2022-10-16 10:54:01 +0300

    csvToJson: Added exception handling to readCsv, storeListInJson.

--- csvToJson/python-csv-to-json.py
+++ csvToJson/python-csv-to-json.py
@@ -5,18 +5,34 @@ import json
 def readCsv(path):
     csvDataList = []
 
-    with open(path, newline='') as csvfile:
-        parsedCsvFile = csv.DictReader(csvfile)
-        for row in parsedCsvFile:
-            csvDataList.append(row)
+    try:
+        with open(path, newline='') as csvfile:
+            parsedCsvFile = csv.DictReader(csvfile)
+            for row in parsedCsvFile:
+                csvDataList.append(row)
+    except FileNotFoundError:
+        print('The requested file does not exist, check file path. File path: ' + path)
+
+        return
 
     return csvDataList
 
 
 def storeListInJson(list, path):
-    with open(path, "a") as file:
-        json.dump(list, fp=file, indent=4)
-        file.close
+    if (list is None):
+        print('List is empty.')
+
+        return
+
+    try:
+        with open(path, "a") as file:
+            json.dump(list, fp=file, indent=4)
+            file.close
+    except json.JSONDecodeError:
+        print('An error occured while dumping list into json file. File path: ' + path)
+        print('Error message: ' + json.JSONDecodeError.msg)
+
+        return
 
 
 parsedData = readCsv("filename.csv")
